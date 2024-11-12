9ff4ed2030d8d842f3e32ff19caca3cfc8fc1213
ChazzitUP <hamzarezgui.education@gmail.com>
2024-11-12 12:26:40 +0100

    activating account component

--- book-network-ui/src/app/pages/activate-account/activate-account.component.ts
+++ book-network-ui/src/app/pages/activate-account/activate-account.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../services/services';
+import { CommonModule } from '@angular/common';
+import { CodeInputModule } from 'angular-code-input';
 
 @Component({
   selector: 'app-activate-account',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CodeInputModule],
   templateUrl: './activate-account.component.html',
-  styleUrl: './activate-account.component.scss'
+  styleUrl: './activate-account.component.scss',
 })
 export class ActivateAccountComponent {
+  message: string = '';
+  isOkay: boolean = true;
+  submitted: boolean = false;
 
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {}
+  redirectToLogin() {
+    this.router.navigate(['login']);
+  }
+  onCodeCompleted(token: string) {
+    this.confirmAccount(token);
+  }
+  confirmAccount(token: string) {
+    this.authService
+      .confirm({
+        token,
+      })
+      .subscribe({
+        next: () => {
+          this.message =
+            'Your account has been activated successfully. \n You can now proceed to login.';
+          this.submitted = true;
+        },
+        error: () => {
+          this.isOkay = false;
+          this.message = 'Token is invalid or expired. Please try again.';
+          this.submitted = true;
+        },
+      });
+  }
 }
